3e956db0ebc657fc375790ed9224c4fee6581860
Edward Castaneda <aeghin7@gmail.com>
2021-11-10 13:28:36 -0600

    entry routes and login

---
+++ controllers/api/entryRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Entry } = require('../../models')
+
+router.post('/entry', async (req, res) => {
+
+    try {
+        const userEntry = await Entry.create({
+            title: req.body.title,
+            text: req.body.text,
+            date: req.body.date,
+            user: req.body.user_id
+        });
+        
+    } catch (err) {
+        res.status(500).json({ message: 'Journal entry failed' });
+    }
+
+});
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,12 +1,12 @@
 const logout = async () => {
-    const response = await fetch('', {
+    const response = await fetch('/logout', {
         method: 'POST', 
         headers: { 'Content-Type': 'application/json'},
     });
 
 
     if (response.ok) {
-        document.location.replace();
+        document.location.replace('/login');
     } else {
         alert(response.statusText);
     }
